38014f7420922eae182e6703e2de66151c01f959
Sergei Prilutskii <sprilut@MacBook-Pro-Sergei.local>
2023-12-25 14:36:32 +0300

    Change Readme fix config and add new files to .gitignore

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import hydra
 from omegaconf import DictConfig
 
-import mlops_project.mlops_project.train_rfr as train_rfr
+import mlops_project.train_rfr as train_rfr
 
 
 @hydra.main(config_path="configs", config_name="config", version_base="1.3")
